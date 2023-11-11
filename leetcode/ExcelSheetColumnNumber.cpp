5e3c16bd84b44dcbb05b03edb26e1e8c377fc910
Prajwal Saokar <prajwalsaokar@gmail.com>
2023-11-11 00:03:09 -0500

    Create ExcelSheetColumnNumber.cpp

---
+++ leetcode/ExcelSheetColumnNumber.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/excel-sheet-column-number/description/
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        unordered_map<char, int> alphaNum;
+        int ans = 0;
+        int i = 1;
+        for(char c = 'A'; c<= 'Z'; c++) {
+            alphaNum[c] = i;
+            i++;
+        }
+
+        for (char c: columnTitle) {
+            ans *= 26;
+            ans += alphaNum[c];
+        }
+        return ans;
+
+
+
+        
+    }
+};
