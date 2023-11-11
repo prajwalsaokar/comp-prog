cd31deda0b14a9b4383e7d98f9944d37ef389adc
Prajwal Saokar <prajwalsaokar@gmail.com>
2023-11-10 22:32:02 -0500

    Create ValidAnagram.cpp

---
+++ leetcode/ValidAnagram.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/valid-anagram/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<char> first(s.begin(), s.end());
+        sort(first.begin(), first.end());
+        vector<char> second(t.begin(), t.end());
+        sort(second.begin(), second.end());
+
+        if(first == second) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+};
