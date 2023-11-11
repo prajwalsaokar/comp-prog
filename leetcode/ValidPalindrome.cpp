ba395f8038350e1fcaf5ed9c3a545ad6f401212a
Prajwal Saokar <prajwalsaokar@gmail.com>
2023-11-10 22:37:04 -0500

    Create ValidPalindrome.cpp

---
+++ leetcode/ValidPalindrome.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/valid-palindrome/
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string p;
+        for(char c: s) {
+            if(isalnum(c)) {
+                p.push_back(tolower(c));
+            }
+        }
+        auto r = p;
+        reverse(p.begin(), p.end());
+
+        if (p == r) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
