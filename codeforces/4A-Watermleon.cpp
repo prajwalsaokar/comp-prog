cbbdf1e650d9251fcf8b029a99c470b8c1e37e27
Prajwal Saokar <prajwalsaokar@gmail.com>
2023-11-13 04:23:03 -0500

    Create 4A-Watermleon.cpp

---
+++ codeforces/4A-Watermleon.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n;
+  cin >> n;
+  if(n >2 & n%2==0) {
+    cout << "YES";
+  }
+  else {
+    cout << "NO";
+  }
+}
